0367750e22dc4ddbc6a98deb756760e2eb507c3b
GADO89 <gadomahmoud377@gmail.com>
2023-11-26 04:41:29 +0200

    Finish TDD

--- src/main/java/com/spring/Player.java
+++ src/main/java/com/spring/Player.java
@@ -5,12 +5,9 @@ import java.util.List;
 
 public class Player {
 
-    String name;
+   // String name;
     private List<String> players = new LinkedList<>();
 
-    public Player(String name) {
-        this.name=name;
-    }
 
     public Player(){
         players.add("Treka");
@@ -21,18 +18,14 @@ public class Player {
 
 
 
-    public Player getName(String name){
+    public String getName(String name){
         for (int i=0;i<players.size();i++){
             if(players.get(i).equals(name)){
-                return new Player(players.get(i));
+                return players.get(i);
             }
         }
         return null;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        Player player= (Player) o;
-        return name.equals(player.name);
-    }
+
 }
--- src/test/java/com/spring/SpringTestApplicationTests.java
+++ src/test/java/com/spring/SpringTestApplicationTests.java
@@ -8,13 +8,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringTestApplicationTests {
 
 	@Test
-	void namePlayerTesting() {
-
+	void namePlayerTesting1() {
 		Player player1=new Player();
 		Player player2=new Player();
-
-		//Assertions.assertEquals(player.getName("Messi"),null);
-	Assertions.assertEquals(player1.getName("Messi"), player2.getName("Messi"));
+		Assertions.assertEquals(player1.getName("Messi"), player2.getName("Messi"));
 	}
+	@Test
+	void namePlayerTesting2() {
+		Player player1=new Player();
+		Player player2=new Player();
+		Assertions.assertEquals(player1.getName("M Salah"), player2.getName("M Salah"));
+	}
+	@Test
+	void namePlayerTesting3() {
+		Player player1=new Player();
+		Player player2=new Player();
+		Assertions.assertEquals(player1.getName("CR7"), player2.getName("CR7"));
+	}
+	@Test
+	void namePlayerTesting4() {
+		Player player1=new Player();
+		Player player2=new Player();
+		Assertions.assertEquals(player1.getName("CR7"),"CR7");
+	}
+
+
 
 }
