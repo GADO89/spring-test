2a19ae53f4525a7888ba05f5f28c5aab8fa55fad
GADO89 <gadomahmoud377@gmail.com>
2023-11-26 03:29:45 +0200

    TDD Example #1

---
+++ src/main/java/com/spring/Player.java
@@ -0,0 +1,29 @@
+package com.spring;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Player {
+
+   // String name;
+    private List<String> players = new LinkedList<>();
+
+    public Player(){
+        players.add("Treka");
+        players.add("Messi");
+        players.add("CR7");
+        players.add("M Salah");
+    }
+
+
+
+    public String getName(String name){
+        for (int i=0;i<players.size();i++){
+            if(players.get(i).equals(name)){
+                return players.get(i);
+            }
+        }
+        return null;
+    }
+
+}
