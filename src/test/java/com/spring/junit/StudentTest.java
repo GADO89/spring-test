10d72b467cd9d28caaa12873e1b3d95fcc378159
GADO89 <gadomahmoud377@gmail.com>
2023-11-28 02:08:05 +0200

    assertTrue

--- src/test/java/com/spring/junit/StudentTest.java
+++ src/test/java/com/spring/junit/StudentTest.java
@@ -10,12 +10,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
+
     @Test
+    void newTest(){
+        Assertions.assertTrue("Esslam".equalsIgnoreCase("ESSLAM"));
+    }
+
+
+
+   /* @Test
     @DisplayName("Test Student  By Id")
     void findById() {
         Student student=new Student();
         //Assertions.assertEquals(student.findById(1),null);
         Assertions.assertThrows(MissingFormatArgumentException.class,
                 ()->student.getException());
-    }
+    }*/
 }
