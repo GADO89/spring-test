3869bcf014fa86b29fe8e2bf9a346671bd3619e1
GADO89 <gadomahmoud377@gmail.com>
2023-11-29 04:34:40 +0200

    @CsvFileSource

--- src/test/java/com/spring/junit/ParamTesting.java
+++ src/test/java/com/spring/junit/ParamTesting.java
@@ -3,6 +3,7 @@ package com.spring.junit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -31,6 +32,18 @@ public class ParamTesting {
         System.out.println("name"+anme);
         System.out.println("age"+age);
         System.out.println("phone"+phone);
+
+    }
+
+
+    @DisplayName("Param Test")
+    @ParameterizedTest(name = "{displayName}: {index} ===> {arguments}")
+   @CsvFileSource(resources = "/mydata.csv", numLinesToSkip = 1)
+    void myParamTesting3(String name, int age, String phone){
+
+        System.out.println("name "+ name);
+        System.out.println("age "+ age);
+        System.out.println("phone "+ phone);
 
     }
 
