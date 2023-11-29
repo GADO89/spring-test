e683566ae423064a9b5f8ae3eacc2af6b8e20839
GADO89 <gadomahmoud377@gmail.com>
2023-11-29 02:27:35 +0200

    @Nested

--- src/test/java/com/spring/junit/StudentTest.java
+++ src/test/java/com/spring/junit/StudentTest.java
@@ -1,25 +1,51 @@
 package com.spring.junit;
 
+import org.assertj.core.api.Assertions;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 
 
 @Tag("student")
 class StudentTest {
 
+    @BeforeEach
+    void setUp() {
+        System.out.println("Out");
+    }
 
     @Test
     void newTest(){
         Student student=new Student();
 
-      //  Assertions.assertThat(student.getName()).isEqualTo("Eslam");
+        //Assertions.assertThat(student.getName()).isEqualTo("Eslam");
         //Assertions.assertTrue("Esslam".equalsIgnoreCase("ESSLAM"));
         MatcherAssert.assertThat(student.getName(), Matchers.is("Eslam"));
     }
 
+    @Nested
+    class NestedStudent {
+        @BeforeEach
+        void setUp() {
+            System.out.println("In");
+        }
 
+        @Test
+        void nestedStudent() {
+            Student student=new Student();
+
+
+            Assertions.assertThat(student.getName()).isEqualTo("Eslam");
+
+        }
+
+    }
+
+    }
 
    /* @Test
     @DisplayName("Test Student  By Id")
@@ -29,4 +55,3 @@ class StudentTest {
         Assertions.assertThrows(MissingFormatArgumentException.class,
                 ()->student.getException());
     }*/
-}
